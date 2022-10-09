f658892df7b3581634b6ee55c14417a894cee875
Raquel Colares <69801403+raquelcolares@users.noreply.github.com>
2022-10-09 19:34:48 -0300

    Add files via upload

---
+++ Code challenges/5 - Polyglot Parrot/polyglot-parrot.py
@@ -0,0 +1,15 @@
+while True: 
+    try: 
+               
+     perna = input() 
+     if (perna == 'esquerda'):
+        print('ingles')
+     elif (perna == 'direita'):
+        print('frances')
+     elif (perna == 'nenhuma'):
+        print('portugues')
+     else:
+        print('caiu')
+    
+    except EOFError: 
+        break
